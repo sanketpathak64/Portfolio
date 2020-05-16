dfc3d26c4d045ef3d6913f57bdb7c8a7e20cfac2
sanketpathak64 <sanketpathak64@gmail.com>
2020-05-17 01:20:51 +0530

    connected github api

---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+const client_id="Iv1.7e06d4ce5e31f020";
+const client_secret="2cca912331bc26cefad4d345f129ff2720b662a7";
+// https://api.github.com/users/sanketpathak64/repos
+const fetchUser = async () => {
+	const api_call = await fetch(`https://api.github.com/users/sanketpathak64/repos?client_id=${client_id}&client_secret=&{client_secret}`);
+	const data = await api_call.json();
+
+	return { data };
+};
+
+const showData = () => {
+	fetchUser().then((res) => {
+		console.log(res);
+		// console.log("hello"+res[0].data.name);
+		console.log("hello"+res.data[0].name);
+		// console.log("hello"+res.data.name[0]);
+		for(var i=0;i<res.data.length;i++)
+		{
+			var node = document.createElement("p");
+			  var textnode = document.createTextNode(res.data[i].name);
+			  node.appendChild(textnode);
+			  
+
+			 var node1 = document.createElement("p");
+			  var textnode = document.createTextNode(res.data[i].description);
+			  node1.appendChild(textnode);
+			  document.getElementById("header").appendChild(node);
+			  document.getElementById("desc").appendChild(node1);
+		} 
+	})
+	
+}
+
+showData();
+
